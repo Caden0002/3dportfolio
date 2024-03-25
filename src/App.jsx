76eee35d45408f484edb6d5e41226f34a09f449d
caden <chen.qixuan97@gmail.com>
2024-03-26 04:24:27 +0800

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,13 @@ export default function App() {
         };
     }, []);
 
+    React.useEffect(() => {
+        var _mtm = window._mtm = window._mtm || [];
+        _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src='https://cdn.matomo.cloud/cadenchen.matomo.cloud/container_moeTbXfI.js'; s.parentNode.insertBefore(g,s);
+    }, [])
+
     return (
         <>
             <LoaderPortfolio3d />
