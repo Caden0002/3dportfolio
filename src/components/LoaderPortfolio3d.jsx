5f106a5bea44ed174d0eccaab5693e9c3a985131
OmeletteX <chen.qixuan97@gmail.com>
2024-05-29 12:25:08 +0800

    “added_text_loader”

--- src/components/LoaderPortfolio3d.jsx
+++ src/components/LoaderPortfolio3d.jsx
@@ -12,7 +12,7 @@ function LoaderPortfolio3d(props) {
             // After 4 seconds (when loading becomes false), start fading out the loader
             controls.start({
                 opacity: 0,
-                transition: { duration: 2}
+                transition: { duration: 2 }
             });
         }
     }, [loading, controls]);
@@ -26,20 +26,21 @@ function LoaderPortfolio3d(props) {
         return () => clearTimeout(timer);
     }, []);
 
+    // Text to be displayed below the helix loader
+    const loadingText = "This page may experience longer loading times due to the presence of 3D models.";
+
     return (
         <motion.div
-            className={`fixed bg-black h-screen top-0 left-0 w-full h-full flex justify-center items-center z-10 ${loading ? '' : 'pointer-events-none'}`}
+            className={`fixed bg-black h-screen top-0 left-0 w-full h-full flex flex-col justify-center items-center z-10 ${loading ? '' : 'pointer-events-none'}`}
             animate={controls}
         >
             {loading ? (
                 // Use the helix loader component conditionally based on the loading state
-                <l-helix size="90" speed="2.5" color="#62FAD7"></l-helix>
-                        ) : (
-                <div>
-                    {/* Add your documentation text here */}
-                    {/* For brevity, I'll omit the full text */}
-                </div>
-            )}
+                <>
+                    <l-helix size="90" speed="2.5" color="#62FAD7"></l-helix>
+                    <div className="text-textColorSecondary mt-12 mx-36 text-xs text-center leading-5 ">{loadingText}</div>
+                </>
+            ) : null}
         </motion.div>
     );
 }
